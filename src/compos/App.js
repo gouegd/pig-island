36678ff4539a85b56a72b6b9e879ae20385c0ebb
Gregory Desfour <gregory.desfour@accedo.tv>
2016-08-27 23:48:23 +0200

    Added images, dynamic nav

--- src/compos/App.js
+++ src/compos/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
+import { nextOf } from '../constants';
 
-const App = () => (
-  <div>Welcome to my little pig page... </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 1,
+      next: nextOf[1]
+    };
+  }
+
+  choose(choice) {
+    this.setState({
+      id: choice,
+      next: nextOf[choice]
+    });
+  }
+
+  render() {
+    const { id, next } = this.state;
+    return (
+      <div>
+        <img src={ `${id}.jpg` } alt={ id } style={ { maxHeight: '90vh' } } />
+        { next.map(item =>
+          <button onClick={ () => this.choose(item) }>{item}</button>
+        ) }
+        { !next.length && <button onClick={ () => this.choose(1) }>Restart</button> }
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  id: PropTypes.number.isRequired,
+  next: PropTypes.array.isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from 'firebase';
+
 import App from './compos/App';
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyCGThJ7PomZDD9K7C82jjzYBQw_yJTqxRI",
-  authDomain: "pigisland-52edc.firebaseapp.com",
-  databaseURL: "https://pigisland-52edc.firebaseio.com",
-  storageBucket: "pigisland-52edc.appspot.com",
+  apiKey: 'AIzaSyCGThJ7PomZDD9K7C82jjzYBQw_yJTqxRI',
+  authDomain: 'pigisland-52edc.firebaseapp.com',
+  databaseURL: 'https://pigisland-52edc.firebaseio.com',
+  storageBucket: 'pigisland-52edc.appspot.com',
 };
 firebase.initializeApp(config);
-
 
 ReactDOM.render(
   <App />,
